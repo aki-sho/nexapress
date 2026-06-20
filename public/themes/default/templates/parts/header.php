c0b984c4f6351e9949ed2a4135ebbb1aeacc90ff
aki-sho <inpalmark@gmail.com>
2026-06-20 22:43:00 +0900

    default Theme削除

--- public/themes/default/templates/parts/header.php
+++ public/themes/default/templates/parts/header.php
@@ -4,9 +4,7 @@
             <h1 class="site-title">
                 <a href="<?= url('') ?>">NexaPress</a>
             </h1>
-            <p class="site-description">default Theme</p>
         </div>
-
         <nav class="site-nav">
             <a href="<?= url('') ?>">トップ</a>
         </nav>
